3005d4590140b64bcf97ade5c61e74a20e5f3fe3
mrgeneralgoo <mrgeneralgoo@gmail.com>
2022-09-04 16:02:56 +0800

    Rollback config

--- handle_feeds.sh
+++ handle_feeds.sh
@@ -15,3 +15,6 @@
 # Add a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+
+sed -i "/lienol/d" "feeds.conf.default"
+ echo "src-git lienol https://github.com/Lienol/openwrt-package.git" >> feeds.conf.default
